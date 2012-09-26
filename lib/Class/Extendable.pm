30ecd7bfb629e3cccf4ebe5012a76f93125b4523
Kentaro Kuribayashi <kentarok@gmail.com>
2012-09-26 23:01:00 +0900

    initial import

---
+++ lib/Class/Extendable.pm
@@ -0,0 +1,39 @@
+package Class::Extendable;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Class::Extendable - blah blah blah
+
+=head1 SYNOPSIS
+
+  use Class::Extendable;
+
+=head1 DESCRIPTION
+
+Class::Extendable is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
